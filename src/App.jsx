91996918d92edb4d2a99c130d53b8450598b368d
ShreyaSupe <shreyasupe510@gmail.com>
2024-02-01 19:32:34 +0530

    Added footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 
 import './App.css'
+import Footer from './compoents/footer'
 
 function App() {
 
 
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <Footer/>
   )
 }
 
